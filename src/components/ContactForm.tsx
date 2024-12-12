7e6ec51d81af3b39476dca72ede9e4ab08f4e8ab
pelu123 <tomascooklastra@gmail.com>
2024-12-12 19:24:08 -0300

    "functional ContactForm"

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,28 +1,69 @@
 import "./components.css"
+import { useState } from "react";
+import emailjs from '@emailjs/browser';
 
 export default function ContactForm() {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const serviceId = 'service_5vw2t4a';
+    const templateId = 'template_whezjtj';
+    const publicKey = '4rrNmxoenTf-hATTF';
+
+    const templateParams = {
+      from_name: name,
+      from_email: email,
+      to_name: 'North Trade',
+      message: message,
+    }
+
+    emailjs.send(serviceId, templateId, templateParams, publicKey)
+      .then((response) => {
+        console.log('Email sent succesfully', response)
+        setName('')
+        setEmail('')
+        setMessage('')
+      })
+      .catch((error) => {
+        console.log('Error sending email', error)
+      })
+  }
+
   return (
     <div className="contact-form-container">
-      <form action="" className="contact-form">
+      <form action="" className="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Nombre</label>
           <input
             type="text"
             id="name"
-            name="name"
+            value={name}
             placeholder="Nombre y apellido"
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
         <div>
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" placeholder="Email" />
+          <input
+            type="email"
+            id="email"
+            value={email}
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+            />
         </div>
         <div>
           <label htmlFor="message">Mensaje</label>
           <textarea
             id="message"
-            name="message"
+            value={message}
             placeholder="Ingrese su mensaje o consulta"
+            onChange={(e) => setMessage(e.target.value)}
           ></textarea>
         </div>
         <div>
